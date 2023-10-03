75500c80d04b48a1de20515b60ab445dbe7b8565
777freezy <109428496+777freezy@users.noreply.github.com>
2023-10-03 01:01:59 -0300

    Create new file, function bluetooht is add

---
+++ NewCode.ino
@@ -0,0 +1,46 @@
+#include <SoftwareSerial.h> 
+
+const int led = 13;
+const int releLeft = 4;
+const int releRight = 5;
+char dado = '0';
+
+ 
+SoftwareSerial bluetooht(2, 3);
+
+void setup() {
+  Serial.begin(9600);
+  bluetooht.begin(9600);
+  pinMode(led, OUTPUT);
+  pinMode(releLeft, OUTPUT);
+  pinMode(releRight, OUTPUT);
+}
+
+void loop() {
+  if(bluetooht.available()){
+    dado = bluetooht.read(); 
+    Serial.println(dado);
+      switch(dado){
+        case '1':
+          digitalWrite(releLeft, HIGH);
+          digitalWrite(releRight, HIGH);
+          break;
+        case '2': 
+          digitalWrite(releLeft, LOW);
+          digitalWrite(releRight, LOW);
+          break;
+        case '3': 
+          digitalWrite(releLeft, LOW);
+          digitalWrite(releRight, HIGH);
+          break;
+        case '4':
+          digitalWrite(releLeft, HIGH);
+          digitalWrite(releRight, LOW);
+          break;
+        default:
+          digitalWrite(releLeft, LOW);
+          digitalWrite(releRight, LOW);
+          break;
+      }
+  }
+}
